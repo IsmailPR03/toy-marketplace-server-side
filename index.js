dd77c0976b6ea426b2ee17a482c566db89a37031
IsmailPR03~ <you@example.com>
2023-05-31 01:24:32 +0600

    connected to client side

--- index.js
+++ index.js
@@ -22,3 +22,27 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    
+
+    const toysCollection = client.db("toys").collection('toyProduct')
+    const allToysCollection = client.db("toys").collection('allToys')
+
+
+    app.get('/toys', async (req, res) => {
+      const query = {}
+      const result = await toysCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    app.get('/toys/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await toysCollection.find(query).toArray()
+      res.send(result)
+    })
+
+   
